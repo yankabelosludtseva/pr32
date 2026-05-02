3d28087f6a44390dff016121ab84583be035e2ee
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-02 14:14:16 +0500

    Разработка окна добавления и изменения пластинки. Белослудцева

---
+++ VinylRecordsApplication/Pages/Records/Add.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VinylRecordsApplication.Pages.Records
+{
+    /// <summary> Логика взаимодействия для Add.xaml
+    public partial class Add : Page
+    {
+        // Обращаемся к поставщикам и получаем всех из БД
+        public IEnumerable<Classes.Manufacturer> Manufacturers = Classes.Manufacturer.AllManufacturers();
+
+        public IEnumerable<Classes.State> AllState = Classes.State.AllState();
+        // Переменная для изменения данных о пластинке
+        private Classes.Record changeRecord;
+
+        public Add(Classes.Record changeRecord = null)
+        {
+            // Инициализируем компоненты для того чтобы с ними работать
+            InitializeComponent();
+            // Перебираем всех поставщиков
+            foreach (var item in Manufacturers)
+                // Добавляем поставщика в выпадающий список
+                tbManufacturer.Items.Add(item.Name);
+            // Если кол-во поставщиков больше 0
+            if (Manufacturers.Count() > 0)
+                // Выбираем первого поставщика
+                tbManufacturer.SelectedIndex = 0;
+            // Добавляем состояния в выпадающий список
+            foreach (var item in AllState)
+                tbState.Items.Add(item.Name);
+            // Если у нас есть состояния
+            if (AllState.Count() > 0)
+                // Выбираем по умолчанию первый
+                tbState.SelectedIndex = 0;
+            // Если пришли данные о пластинке на изменение
+            if (changeRecord != null)
+            {
+                // Сохраняем данные о пластинке
+                this.changeRecord = changeRecord;
+                // Выводим наименование
+                tbName.Text = changeRecord.Name;
+                // Выводим год
+                tbYear.Text = changeRecord.Year.ToString();
+                // Выводим стоимость
+                tbPrice.Text = changeRecord.Price.ToString().Replace(",", ".");
+                // Выводим описание
+                tbDescription.Text = changeRecord.Description;
+                // Выбираем формат
+                tbFormat.SelectedIndex = changeRecord.Format;
+                // Выбираем поставщика получая его по уникальному коду
+                tbManufacturer.SelectedIndex = Manufacturers.ToList().FindIndex(x => x.Id == changeRecord.IdManufacturer);
+                // Выбираем размер
+                tbSize.SelectedIndex = changeRecord.Size;
+                // Выбираем состояние
+                tbState.SelectedIndex = AllState.ToList().FindIndex(x => x.Id == changeRecord.IdState);
+                // Изменяем название кнопки с "Добавить" на "Изменить"
+                addBth.Content = "Изменить";
+            }
+        }
+
+        /// <summary> Добавление/Изменение пластинки
+        private void AddRecord(object sender, RoutedEventArgs e)
+        {
+            // Проверяем что поле наименования не пустое
+            if (!String.IsNullOrEmpty(tbName.Text))
+                // Проверяем что поле года не пустое
+                if (!String.IsNullOrEmpty(tbYear.Text))
+                    // Проверяем что поле цены не пустое
+                    if (!String.IsNullOrEmpty(tbPrice.Text))
+                        // Проверяем что наименование меньше 250 символов
+                        if (tbName.Text.Length <= 250)
+                        {
+                            // Добавление записи
+                            if (changeRecord == null)
+                            {
+                                // Создаём новый экземпляр класса пластинки, указывая данные
+                                Classes.Record newRecord = new Classes.Record()
+                                {
+                                    // Наименование
+                                    Name = tbName.Text,
+                                    // Год
+                                    Year = Convert.ToInt32(tbYear.Text),
+                                    // Формат
+                                    Format = tbFormat.SelectedIndex,
+                                    // Размер
+                                    Size = tbSize.SelectedIndex,
+                                    // Производитель
+                                    IdManufacturer = Manufacturers.Where(x => x.Name == tbManufacturer.SelectedValue.ToString()).First().Id,
+                                    // Стоимость
+                                    Price = float.Parse(tbPrice.Text.Replace(".", ",")),
+                                    // Состояние
+                                    IdState = AllState.Where(x => x.Name == tbState.SelectedItem.ToString()).First().Id,
+                                    // Описание
+                                    Description = tbDescription.Text
+                                };
+                                // Сохраняем данные
+                                newRecord.Save();
+                                // Выводим сообщение
+                                MessageBox.Show($"Пластинка {newRecord.Name} успешно добавлена.", "Уведомление");
+                                // Открываем пластинку на изменение
+                                MainWindow.mainWindow.OpenPage(new Pages.Records.Add(newRecord));
+                            }
+                            else
+                            {
+                                // Если изменение
+                                // Подхватываем данные с полей
+                                changeRecord.Name = tbName.Text;
+                                changeRecord.Year = Convert.ToInt32(tbYear.Text);
+                                changeRecord.Format = tbFormat.SelectedIndex;
+                                changeRecord.Size = tbSize.SelectedIndex;
+                                changeRecord.IdManufacturer = Manufacturers.Where(x => x.Name == tbManufacturer.SelectedValue.ToString()).First().Id;
+                                changeRecord.Price = float.Parse(tbPrice.Text.Replace(".", ","));
+                                changeRecord.IdState = AllState.Where(x => x.Name == tbState.SelectedItem.ToString()).First().Id;
+                                changeRecord.Description = tbDescription.Text;
+                                // Сохраняем данные
+                                changeRecord.Save(true);
+                                // Выводим сообщение
+                                MessageBox.Show($"Пластинка {changeRecord.Name} успешно изменена.", "Уведомление");
+                            }
+                        }
+                        else
+                            // Выводим сообщение об ошибке
+                            MessageBox.Show("Наименование пластинки слишком большое.", "Предупреждение");
+                    else
+                        // Выводим сообщение об ошибке
+                        MessageBox.Show("Пожалуйста, укажите стоимость пластинки.", "Предупреждение");
+                else
+                    // Выводим сообщение об ошибке
+                    MessageBox.Show("Пожалуйста, укажите год выпуска пластинки.", "Предупреждение");
+            else
+                // Выводим сообщение об ошибке
+                MessageBox.Show("Пожалуйста, укажите наименование пластинки.", "Предупреждение");
+        }
+
+        /// <summary> Функция проверки на цифры
+        private void tbPreviewNumber(object sender, TextCompositionEventArgs e)
+        {
+            // если в строке используется текст, запрещаем ввод
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
+
+        /// <summary> Функция проверки на цифры с точкой
+        private void tbPreviewFloat(object sender, TextCompositionEventArgs e)
+        {
+            // если в строке используется текст, запрещаем ввод
+            e.Handled = !(Char.IsDigit(e.Text, 0)) && e.Text != ".";
+        }
+    }
+}
--- VinylRecordsApplication/Pages/Records/Elements/Record.xaml.cs
+++ VinylRecordsApplication/Pages/Records/Elements/Record.xaml.cs
@@ -1,28 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VinylRecordsApplication.Pages.Records.Elements
 {
-    /// <summary>
-    /// Логика взаимодействия для Record.xaml
-    /// </summary>
+    /// <summary> Логика взаимодействия для Record.xaml
     public partial class Record : UserControl
     {
-        public Record()
+        /// <summary> Список состояний
+        private IEnumerable<Classes.State> AllState = Classes.State.AllState();
+        /// <summary> Данные о пластинке
+        private Classes.Record record;
+        /// <summary> Ссылка на страницу Маин
+        private Pages.Records.Main main;
+
+        public Record(Classes.Record record, Pages.Records.Main main)
         {
+            // Инициализируем компоненты для того чтобы с ними работать
             InitializeComponent();
+            // получаем данные о поставщиках
+            IEnumerable<Classes.Manufacturer> AllManufacturer = Classes.Manufacturer.AllManufacturers();
+            // Запоминаем данные о пластинке
+            this.record = record;
+            // Запоминаем данные о Маин
+            this.main = main;
+            // Выводим имя
+            tbName.Text = record.Name;
+            // Выводим год
+            tbYear.Text = record.Year.ToString();
+            // Выводим формат
+            tbFormat.Text = record.Format == 0 ? "Моно" : "Стерео";
+            // Выводим размер
+            switch (record.Size)
+            {
+                case 0:
+                    tbSize.Text = "7 дюймов";
+                    break;
+                case 1:
+                    tbSize.Text = "10 дюймов";
+                    break;
+                case 2:
+                    tbSize.Text = "12 дюймов";
+                    break;
+                case 3:
+                    tbSize.Text = "Иной";
+                    break;
+            }
+            // Выводим поставщика
+            tbManufacturer.Text = AllManufacturer.Where(x => x.Id == record.IdManufacturer).First().Name;
+            // Выводим стоимость
+            tbPrice.Text = record.Price.ToString();
+            // Выбираем состояние
+            tbState.Text = AllState.Where(x => x.Id == record.IdState).First().Name;
+            // Выводим описание
+            tbDescription.Text = record.Description;
+        }
+
+        /// <summary> Метод изменения
+        private void EditRecord(object sender, RoutedEventArgs e)
+        {
+            // Обращаемся к главной форме
+            // Открываем страницу добавления передавая данные о пластинке => получаем страницу изменения данных
+            MainWindow.mainWindow.OpenPage(new Add(this.record));
+        }
+
+        /// <summary> Удаление данных
+        private void DeleteRecord(object sender, RoutedEventArgs e)
+        {
+            // Спрашиваем пользователя
+            if (MessageBox.Show($"Удалить виниловую пластинку: {this.record.Name}?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                // Получаем поставки
+                IEnumerable<Classes.Supply> AllSupply = Classes.Supply.AllSupplies();
+                // Если среди поставок есть наша пластинка
+                if (AllSupply.Where(x => x.IdRecord == record.Id).Count() > 0)
+                {
+                    // Выводим уведомление о том что нельзя удалить
+                    MessageBox.Show($"Виниловую пластинку {this.record.Name} невозможно удалить. Для начала удалите зависимости.", "Уведомление");
+                }
+                else
+                {
+                    // Удаляем запись
+                    this.record.Delete();
+                    // Удаляем элемент с интерфейса
+                    main.recordsParent.Children.Remove(this);
+                    // Выводим сообщение
+                    MessageBox.Show($"Пластинка {this.record.Name} успешно удалена.", "Уведомление");
+                }
+            }
         }
     }
 }
